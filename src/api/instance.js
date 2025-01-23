9a70052d0f8ad9e07373b42ffa914623a37367ef
Abdullah <abdullahvora136@gmail.com>
2025-01-23 17:52:11 +0530

    .

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const apiInstance = axios.create({
-    baseURL: 'https://fur-store-api.vercel.app',
+    // baseURL: 'https://fur-store-api.vercel.app',
+    baseURL: 'https://furstore-api.onrender.com',
+    withCredentials: true
 })
 
 export default apiInstance;
